6035a8d0445761df45dc92e4ecb29ee468685bf4
jixiongge <248273785@qq.com>
2015-01-14 11:15:42 +0800

    hualin2
    hualin2

---
+++ tetetet/123.h
@@ -0,0 +1,13 @@
+//
+//  123.h
+//  tetetet
+//
+//  Created by jixiongge on 15/1/14.
+//  Copyright (c) 2015年 jixiongge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface _23 : NSObject
+
+@end
---
+++ tetetet/123.m
@@ -0,0 +1,13 @@
+//
+//  123.m
+//  tetetet
+//
+//  Created by jixiongge on 15/1/14.
+//  Copyright (c) 2015年 jixiongge. All rights reserved.
+//
+
+#import "123.h"
+
+@implementation _23
+
+@end
